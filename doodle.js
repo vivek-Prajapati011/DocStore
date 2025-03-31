a51f107d26e250d5a986b19a3e277901d383cd97
vivek prajapati <vivek011prajapati@gmail.com>
2025-04-01 04:59:12 +0530

    coding and fix some errors

--- doodle.js
+++ doodle.js
@@ -1,53 +1,62 @@
-import { readdir, readFile, open } from "fs/promises"
-import http from "node:http"
-import fileHandle from "node:fs/promises"
+import { open, readdir, readFile } from "fs/promises";
+import http from "http";
+import mime from "mime-types";
 
-const server = http.createServer(async(request, response) => {
-  if(request.url === "/favicon.ico") return response.end("no favicon")
-  if(request.url === "/"){
-   servDirectory(request, response)
-  }
-  else{
-   try{
-    const [url, queryString] = request.url.split("?")
-    console.log({url, queryString})
-    const fileHandle = await open(`./storage${decodeURIComponent(url) }`)
-    const stat = await fileHandle.stat()
-    if(stat.isDirectory()){
-      servDirectory(request, response)
-    }else{
-      const readstream =  fileHandle.createReadStream()
-      readstream.pipe(response)
-  
-    }
-    
-  }catch(err)
-  {
-    console.log(err.message)
-    response.end ("Not found")
+const server = http.createServer(async (req, res) => {
+  console.log(req.url);
 
-  }
-   }
-})
-
-async function servDirectory(request, response) {
-  const [url, queryString] = request.url.split("?")
-    console.log({url, queryString})
-  const fileHandle = await open(`./storage${decodeURIComponent(url) }`)
-    const stat = await fileHandle.stat()
-    if(stat.isDirectory()){
-      const itemList =  await readdir(`./storage${request.url}`)
-      console.log("Files in storage:", itemList) 
-      let dynamicHTML = ""
-      itemList.forEach((item) => {
-         dynamicHTML +=  `${item} <a href = "${request.url === "/" ? "" : request.url}/${item}">open </a><br><a href = "${request.url === "/" ? "" : request.url}/${item}">Downlod </a><br>`
+  if (req.url === "/favicon.ico") return res.end("No favicon.");
+  if (req.url === "/") {
+    serveDirectory(req, res);
+  } else {
+    try {
+      const [url, queryString] = req.url.split("?");
+      const queryParam = {};
+      queryString.split("&").forEach((pair) => {
+        const [key, value] = pair.split("=");
+        queryParam[key] = value;
       });
-      console.log(dynamicHTML)
-      const boilerHTML = await readFile("./bolierplate.html", "utf-8")
-    response.end(boilerHTML.replace("${dynamicHTML}",dynamicHTML))
-}
+      console.log(queryParam);
+
+      const fileHandle = await open(`./storage${decodeURIComponent(url)}`);
+      const stats = await fileHandle.stat();
+      if (stats.isDirectory()) {
+        serveDirectory(req, res);
+      } else {
+        const readStream = fileHandle.createReadStream();
+        res.setHeader("Content-Type", mime.contentType(url.slice(1)));
+        res.setHeader("Content-Length", stats.size)
+        if (queryParam.action === "download") {
+          res.setHeader(
+            "Content-Disposition",
+            `attachment; filename="${url.slice(1)}"`
+          );
+        }
+        readStream.pipe(res);
+      }
+    } catch (err) {
+      console.log(err.message);
+      res.end("Not Found!");
+    }
+  }
+});
+
+async function serveDirectory(req, res) {
+  const [url, queryString] = req.url.split("?");
+  console.log("hiii", { url, queryString });
+  const itemsList = await readdir(`./storage${url}`);
+  let dynamicHTML = "";
+  itemsList.forEach((item) => {
+    dynamicHTML += `${item} <a href=".${
+      req.url === "/" ? "" : req.url
+    }/${item}?action=open">Open</a> <a href=".${
+      req.url === "/" ? "" : req.url
+    }/${item}?action=download">Download</a><br>`;
+  });
+  const htmlBoilerplate = await readFile("./boilerplate.html", "utf-8");
+  res.end(htmlBoilerplate.replace("${dynamicHTML}", dynamicHTML));
 }
 
 server.listen(80, "0.0.0.0", () => {
-    console.log("Server is running ")
-})
+  console.log("Server started");
+});
