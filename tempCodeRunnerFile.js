7107abaf694e13c194fbd08354a1208467629dd5
vivek prajapati <vivek011prajapati@gmail.com>
2025-04-01 05:01:24 +0530

    done

---
+++ tempCodeRunnerFile.js
@@ -0,0 +1,59 @@
+import { readdir, readFile, open } from "fs/promises"
+import http from "node:http"
+import fileHandle from "node:fs/promises"
+
+const server = http.createServer(async(request, response) => {
+  if(request.url === "/favicon.ico") return response.end("no favicon")
+  if(request.url === "/"){
+   servDirectory(request, response)
+  }
+  else{
+   try{
+    const [url, queryString] = request.url.split("?")
+    const queryParam = {}
+    queryString.split("&").forEach((pair) => {
+      const[key, value] = pair.split("=")
+      queryParam[key] = value
+    })
+    console.log(queryParam)
+
+    const fileHandle = await open(`./storage${decodeURIComponent(url) }`)
+    const stat = await fileHandle.stat()
+    if(stat.isDirectory()){
+      servDirectory(request, response)
+    }else{
+      const readstream =  fileHandle.createReadStream()
+      readstream.pipe(response)
+  
+    }
+    
+  }catch(err)
+  {
+    console.log(err.message)
+    response.end ("Not found")
+
+  }
+   }
+})
+
+async function servDirectory(request, response) {
+  const [url, queryString] = request.url.split("?")
+    console.log({url, queryString})
+  const fileHandle = await open(`./storage${decodeURIComponent(url) }`)
+    const stat = await fileHandle.stat()
+    if(stat.isDirectory()){
+      const itemList =  await readdir(`./storage${request.url}`)
+      console.log("Files in storage:", itemList) 
+      let dynamicHTML = ""
+      itemList.forEach((item) => {
+         dynamicHTML +=  `${item} <a href = "${request.url === "/" ? "" : request.url}/${item}">open </a><br><a href = "${request.url === "/" ? "" : request.url}/${item}">Downlod </a><br>`
+      });
+      console.log(dynamicHTML)
+      const boilerHTML = await readFile("./bolierplate.html", "utf-8")
+    response.end(boilerHTML.replace("${dynamicHTML}",dynamicHTML))
+}
+}
+
+server.listen(80, "0.0.0.0", () => {
+    console.log("Server is running ")
+})
